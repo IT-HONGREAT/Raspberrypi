6034cce6555f674a8205f6f7fd0f6a9ddc8d9348
IT-HONGREAT <hongreat95@gmail.com>
2021-11-16 15:19:22 +0900

    raspberrypi's weights and yolo model, for versions

---
+++ Firstproject_for_versions/app.py
@@ -0,0 +1,144 @@
+import base64
+import io
+import os
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
+from flask import Flask, request, render_template
+from flask_ngrok import run_with_ngrok
+
+confthres = 0.3
+nmsthres = 0.1
+yolo_path = './'
+
+
+def get_labels(labels_path):
+    lpath = os.path.sep.join([yolo_path, labels_path])
+    LABELS = open(lpath).read().strip().split("\n")
+    return LABELS
+
+
+def get_colors(LABELS):
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+    return COLORS
+
+
+def get_weights(weights_path):
+    weightsPath = os.path.sep.join([yolo_path, weights_path])
+    return weightsPath
+
+
+def get_config(config_path):
+    configPath = os.path.sep.join([yolo_path, config_path])
+    return configPath
+
+
+def load_model(configpath, weightspath):
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
+    return net
+
+
+def image_to_byte_array(image:Image):
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format='PNG')
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+
+def get_predection(image, net, LABELS, COLORS):
+    (H, W) = image.shape[:2]
+
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    print(layerOutputs)
+    end = time.time()
+
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > confthres:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres, nmsthres)
+
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            print(boxes)
+            print(classIDs)
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    return image
+
+
+labelsPath = "weights/obj_c2.names"
+cfgpath = "weights/yolov3_china2.cfg"
+wpath = "weights/yolov3_11.09_final.weights"
+
+Lables = get_labels(labelsPath)
+CFG = get_config(cfgpath)
+Weights = get_weights(wpath)
+nets = load_model(CFG, Weights)
+Colors = get_colors(Lables)
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def main():
+    img = request.files["file"].read()
+    img = Image.open(io.BytesIO(img))
+    npimg = np.array(img)
+    image = npimg.copy()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    res = get_predection(image, nets, Lables, Colors)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+    np_img = Image.fromarray(image)
+    img_encoded = image_to_byte_array(np_img)
+    base64_bytes = base64.b64encode(img_encoded).decode("utf-8")
+    return render_template('index.html', user_image=base64_bytes)
+
+
+# start flask app
+if __name__ == '__main__':
+    app.run()
+
+# host='0.0.0.0', port=3000, debug=True
---
+++ Firstproject_for_versions/input_weight.py
@@ -0,0 +1,5 @@
+import subprocess
+from glob import glob
+from pathlib import Path
+
+exec(Path("/home/pi/Downloads/raspberryproject001/pythonProject001/hx711py/example.py").read_text())
