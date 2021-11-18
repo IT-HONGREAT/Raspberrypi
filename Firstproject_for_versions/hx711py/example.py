4398d877ff8533ff9c3f78b319ca5f05d95d77bd
IT-HONGREAT <hongreat95@gmail.com>
2021-11-18 14:14:45 +0900

    final_page, end of prototype.(But i need more powerful page, should contant to django project)

--- Firstproject_for_versions/hx711py/example.py
+++ Firstproject_for_versions/hx711py/example.py
@@ -2,7 +2,6 @@
 
 import time
 import sys
-import pandas as pd
 
 
 EMULATE_HX711=False
@@ -26,21 +25,8 @@ def cleanAndExit():
 
 hx = HX711(20, 16)
 
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
 hx.set_reading_format("MSB", "MSB")
 
-# HOW TO CALCULATE THE REFFERENCE UNIT
-# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
 hx.set_reference_unit(referenceUnit)
 
 hx.reset()
@@ -49,27 +35,13 @@ hx.tare()
 
 print("Tare done! Add weight now...")
 
-# to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
-#---vscode change---
-#data = [] 
-#data_id = 0
+
 def make_weight():
-    # global data_weight
+
     data_weight = 0
     count = 0
     while True:
         try:
-            # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-            # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-            # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
-
-            # np_arr8_string = hx.get_np_arr8_string()
-            # binary_string = hx.get_binary_string()
-            # print binary_string + " " + np_arr8_string
-
-            # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
             val = hx.get_weight()
             ave_weight = 724
 
@@ -83,25 +55,15 @@ def make_weight():
                 count += 1
                 print("weight : ", output_weight)
                 if count == 2:
-                    #data.append([data_id,output_weight])
-                    #data_id += 1
                     data_weight = output_weight
+                    return data_weight
 
-            #print("weights",data)
-            #df = pd.DataFrame(data)
-            #df.to_csv('sample.csv')
+
                 print('data_weight:',data_weight)
-            if data_weight >=10:
-                # break
-
-                return data_weight
-
-            # To get weight from both channels (if you have load cells hooked up
-            # to both channel A and B), do something like this
-            #val_A = hx.get_weight_A(5)
-            #val_B = hx.get_weight_B(5)
-            #print "A: %s  B: %s" % ( val_A, val_B )
-
+            # if data_weight >=10:
+            #     # break
+            #
+            #     return data_weight
             hx.power_down()
             hx.power_up()
             time.sleep(0.1)
@@ -109,7 +71,6 @@ def make_weight():
         except (KeyboardInterrupt, SystemExit):
             cleanAndExit()
 
-    # return data_weight
 make_weight()
 
 
